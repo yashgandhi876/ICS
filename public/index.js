d785eb748d7e71219e3efede92c22ce7a8b68ffd
yash gandhi <yashgandhi2020@gmail.com>
2021-05-27 19:20:33 +0530

    added files for XSS

---
+++ public/index.js
@@ -0,0 +1,45 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click',(e)=>{
+    const data = document.querySelector(".inputData");
+    console.dir(data.value);
+    document.querySelector(".showData").innerHTML =  `you typed: ${String(data.value)}`;
+    // document.querySelector(".showData").innerHTML = `you typed ${data.value}`;
+
+})
+
+
+
+// const root = document.querySelector("#root")
+// const card = document.createElement("div");
+// card.classList.add("w-10/12");
+// card.classList.add("font-bold");
+// card.classList.add("m-auto");
+// card.classList.add("p-4");
+// const cardData = document.createElement('p');
+// cardData.innerHTML = "I am yash Gandhi from pune";
+// card.appendChild(cardData);
+// root.appendChild(card);
+
+{/* <script>
+
+const data = `<div class="w-11/12 m-auto p-4 shadow-lg bg-white">
+                    <div>
+                        <img src="./hacked.jpg" alt="hacekd img" width="500" height=500>
+                    </div>
+                    <div class="flex-col justify-center font-bold">
+                        <h1>
+                            you are hacked
+                        </h1>
+                        <h2>
+                            send bitcoin to this place : 145ycjkmJGhypj6whkexKjmscRb2im53mC
+                        </h2>
+                    </div>
+                </div>`;
+
+const root = document.querySelector(".hacked-card");
+const card = document.createElement('div');
+card.innerHTML= data;
+
+root.appendChild(card);
+</script> */}
